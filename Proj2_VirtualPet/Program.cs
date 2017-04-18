2d6df702e6174c50cabc92652ec4daec64deb3b7
Cthompson16 <Cthompson1646@gmail.com>
2017-04-18 18:33:05 -0400

    Proj2 Virtual Pet

---
+++ Proj2_VirtualPet/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj2_VirtualPet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //this is how we instantiate a new instance of our pet
+            //I did the exact same thing before I don't know why it isn't working this time.
+            //I believe this is what is causing problems in my method below. When cases are listed.
+            VirtualPet myPet = new VirtualPet;
+
+            //Greeting:This will be a welcoming to the game. 
+            Console.WriteLine("Welcome, I am happy you decided to come and play our game!");
+            Console.WriteLine("Your job is to take care of a puppy that needs a new home!");
+            Console.WriteLine("Make sure you pay attention to all of it's needs, good luck!\n");
+
+            //User is prompted to name pet.
+            Console.WriteLine("What will you name your new puppy?");
+            Console.ReadLine();
+
+            //Interactions: Gives user options to care for the pet
+            Console.WriteLine();
+            Console.WriteLine("Please select an option:\n");
+            Console.WriteLine("1.Feed the puppy");
+            Console.WriteLine("2.Play with puppy");
+            Console.WriteLine("3.Give puppy water");
+            Console.WriteLine("4.Take puppy to bathroom");
+            Console.WriteLine("5.Bathe puppy");
+            Console.WriteLine("6.Quit\n");
+
+            //Call Method-Originally used on template
+         
+             myPet.MyPetStatus();
+
+            //There are 6 options for the pet so I set the variable to 6
+            int selectedOption = 6;
+
+            //Using a switch statement because we are using a sequence of case statements
+            //Default statement will be used at the end-It is used to route the flow of the method if a case doesn't match
+                       
+           switch (selectedOption)
+            {
+                case 1:
+
+                    myPet.HungerDecrease();
+                    Console.WriteLine("Thank you for feeding the pet. He loves to eat!");
+                    break;
+
+                case 2:
+
+                    myPet.BoredomIncreases();
+                    Console.WriteLine("Thank you for playing with the pet!");
+
+                case 3:
+
+                    myPet.ThirstIncreases();
+                    Console.WriteLine("Thank you for the water!");
+
+                case 4:
+
+                    myPet.ThirstDecreases();
+                    Console.WriteLine("Thanks for taking me on a walk!");
+
+                case 5:
+
+                    myPet.BoredomDecreased();
+                    Console.WriteLine("Feelin'fresh and so clean, clean. Thanks!");
+                    
+                case 6:
+                                   
+                    Console.WriteLine("Thanks for playing!");
+                    break;
+
+                default:
+
+                    Console.WriteLine("Invalid option selected.");
+                    break;
+            }
+
+
+
+         }
+    }
+}
---
+++ Proj2_VirtualPet/VP_Functions.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj2_VirtualPet
+{
+    class VP_Functions
+    {
+        //Set my fields: Fields are always private. 
+        //Set fields to private so only the VirtualPet class can modify them directly
+        private string name;
+        private int feed;
+        private int boredom;
+        private int thirsty;
+        private int urinate;
+        private int dirty;
+
+        { 
+            this.name = name;
+            this.feed = feed;
+            this.boredom = boredom;
+            this.thirsty = thirsty;
+            this.urinate = urinate; 
+            this.dirty = dirty;
+        }
+
+    //Set the properties-public 
+    //Still have no clue why I am still getting errors, I did the exact same thing as before. 
+    public VirtualPet()
+    {
+        this.name = "name";
+        this.feed = 0;
+        this.boredom = 2;
+        this.thirsty = 10;
+        this.dirty = 3;
+    }
+
+    //method for stats of virtual pet
+    public void MyPetStatus()
+
+    {
+        //Here is the loop to bring all the stats together and make the numbers vary based on what user pics
+        //I coulfn't find a way to list them all together and make the numbers alternate. So it is done individually.
+
+        //Loop which makes emotions increase or decrease based on users options
+                public void FeedIncrease()
+    {
+
+        this.feed = feed + 7;
+    }
+
+    public void HungerDecrease()
+
+    {
+
+        this.hunger = hunger - 6;
+    }
+
+    public void BoredomIncrease()
+
+    {
+        this.boredom = boredom + 5;
+    }
+
+    public void BoredomDecrease()
+
+    {
+        this.boredom = boredom - 5;
+    }
+
+    public void DirtyDecrease()
+
+    {
+        this.dirty = dirty - 4;
+
+    }
+
+    public void DirtyIncrease()
+
+    {
+        this.dirty = dirty + -2;
+    }
+
+    public void ThirstyDecrease()
+
+    {
+        this.thirsty = thirsty - 5;
+    }
+
+    public void ThirstyIncrease()
+
+    {
+        this.thirsty = thirsty -3;
+    }
+
+    Console.WriteLine(name);
+        Console.WriteLine("feed: " + feed);
+        Console.WriteLine("boredom:" + boredom);
+        Console.WriteLine("thirsty:" + thirsty);
+        Console.WriteLine("dirty:" + dirty);
+
+//I have no clue why I have so many errors. I thought I coved everything.
+
+
+
+
+
+
+    }
+
+}
